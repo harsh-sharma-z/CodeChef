ec1461ce32fd559fae64e293f663491a6549326e
Harsh Sharma <80146213+harsh-sharma-z@users.noreply.github.com>
2022-08-10 20:46:08 +0530

    Create Final Population.cpp

---
+++ Starters 51/Final Population.cpp
@@ -0,0 +1,37 @@
+//Harsh Sharma's Code 
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define f(i,a,b) for(long long i=a;i<b;i++)
+#define rf(i,a,b) for(long long i=a;i>=b;i--)
+#define ll long long
+#define pb push_back
+#define vll vector<long long>
+#define vc vector
+#define fi first
+#define se second
+#define pqmin priority_queue<ll,vector<ll>,greater<ll>>
+#define all(x) x.begin(),x.end()
+#define IOS ios_base::sync_with_stdio(0)
+#define tie cin.tie(NULL),cout.tie(NULL) 
+
+//This is a test file to create a cpp program
+//It can hold minor peices of code to run for the instance.
+
+
+
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int x,y,z;
+        cin>>x>>y>>z;
+
+        cout<<(x-y+z)<<endl;
+        
+
+    }
+    return 0;
+}
